84e49097619ed3769aec4a323f764180ed474d23
katvarf <varfolomeeva_97@bk.ru>
2022-10-01 02:18:46 +0300

    add Task025 solution

---
+++ Task025/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25
+// Используя определение степени числа, напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
+
+double Pow(int A, int B)
+{
+    double result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result *= A;
+    }
+    return result;
+}
+
+Console.Write("Введите число A = ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B = ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{A} в степени {B} = {Pow(A, B)}");
