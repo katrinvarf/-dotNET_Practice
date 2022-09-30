d20417ebb4940b01a29b0ec3f488c08e62268370
katvarf <varfolomeeva_97@bk.ru>
2022-10-01 02:19:49 +0300

    add Task029 solution

---
+++ Task029/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 29
+// Напишите программу, которая задаёт массив из 8 случайных целых чисел и выводит отсортированный по модулю массив.
+
+void FillArray(int[] array, int border)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-border, border);
+    }
+}
+
+void SortArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int min = array[i];
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (Math.Abs(min) > Math.Abs(array[j]))
+            {
+                array[i] = array[j];
+                array[j] = min;
+                min = array[i];
+            }
+        }
+    }
+}
+
+int size = 8, border = 100;
+int[] array = new int[size];
+
+FillArray(array, border);
+Console.WriteLine($"Сгенерированный массив: [{String.Join(", ", array)}]");
+
+SortArray(array);
+Console.WriteLine($"Отсортированный массив: [{String.Join(", ", array)}]");
