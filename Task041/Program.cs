a4e56b8e5f28236bcce0613229312e8162a7bb00
katvarf <varfolomeeva_97@bk.ru>
2022-10-09 23:04:39 +0300

    add Task041 solution

---
+++ Task041/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 41
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите числа через Enter:");
+int count = 0;
+string inputText = "";
+
+while ((inputText = Console.ReadLine()) != "")
+{
+    int number = Convert.ToInt32(inputText);
+    if (number > 0) count++;
+}
+
+Console.WriteLine($"Количество чисел > 0: {count}");
