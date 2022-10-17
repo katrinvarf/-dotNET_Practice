df1973fbbbe9fc82bcd029162d45e2beae99dd7f
katvarf <varfolomeeva_97@bk.ru>
2022-10-18 02:31:26 +0300

    add Task064 solution

---
+++ Task064/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64
+// Задайте значения M и N. Напишите рекурсивный метод, который выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+
+void GetNumbers(int a, int b)
+{
+    if (a < b)
+    {
+        if (a % 3 == 0)
+            Console.Write(a + " ");
+        GetNumbers(a + 1, b);
+    }
+    else
+        Console.WriteLine();
+}
+
+Console.WriteLine("Введите значения:");
+Console.Write("M = ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+GetNumbers(M, N);
+
